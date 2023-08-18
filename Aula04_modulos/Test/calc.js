b0bdd4fa04a4913778f5bb0f0218c445261dd3fd
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2023-08-18 04:52:21 -0300

    Aulas 01 a 06

---
+++ Aula04_modulos/Test/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./somar")
+var SubFunc = require("./sub")
+var MultiFunc = require("./multi")
+var DivFunc = require("./div")
+
+console.log(SomaFunc(1, 2))
+console.log(SubFunc(4, 2))
+console.log(MultiFunc(2, 5))
+console.log(DivFunc(10, 2))
